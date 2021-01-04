edaaf6b076f7a39284dbb40bfe403f1f92d1b644
Alex Griffin <alexgriffin.dev@gmail.com>
2021-01-04 15:20:42 +0000

    Got rid of strange WPF GUI error

--- RockPaperScissorsGame/RockPaperScissorsGUI/RockPaperScissorsWindow.xaml.cs
+++ RockPaperScissorsGame/RockPaperScissorsGUI/RockPaperScissorsWindow.xaml.cs
@@ -12,17 +12,29 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using RockPaperScissorsGame;
 
 namespace RockPaperScissorsGUI
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for RockPaperScissorsWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class RockPaperScissorsWindow : Window
     {
-        public MainWindow()
+        GameLoop gl = new GameLoop();
+        public RockPaperScissorsWindow()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            gl.StartUp(NameBox.Text);
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            gl.PlayerRock();
         }
     }
 }
--- RockPaperScissorsGame/RockPaperScissorsGame/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace RockPaperScissorsGame
-{
-    public class Class1
-    {
-    }
-}
