91d40e46e70181d6e081b10a08732d74a69b931f
Alex Griffin <alexgriffin.dev@gmail.com>
2021-01-04 18:04:46 +0000

    Mostly Implemented GUI

--- RockPaperScissorsGame/RockPaperScissorsGUI/RockPaperScissorsWindow.xaml.cs
+++ RockPaperScissorsGame/RockPaperScissorsGUI/RockPaperScissorsWindow.xaml.cs
@@ -22,19 +22,44 @@ namespace RockPaperScissorsGUI
     public partial class RockPaperScissorsWindow : Window
     {
         GameLoop gl = new GameLoop();
+
         public RockPaperScissorsWindow()
         {
             InitializeComponent();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            gl.StartUp(NameBox.Text);
-        }
-
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
             gl.PlayerRock();
+        }
+
+        private void NameSubmitButton_Click(object sender, RoutedEventArgs e)
+        {
+            gl.StartUp(NameBox.Text);
+            HideIntroInfo();
+            ShowGameButtons();
+        }
+
+        private void HideIntroInfo()
+        {
+            IntroductionText.Visibility = Visibility.Hidden;
+            IntroductionText2.Visibility = Visibility.Hidden;
+            IntroductionText3.Visibility = Visibility.Hidden;
+            IntroductionText4.Visibility = Visibility.Hidden;
+            IntroductionText5.Visibility = Visibility.Hidden;
+            IntroductionText6.Visibility = Visibility.Hidden;
+            IntroductionText7.Visibility = Visibility.Hidden;
+            IntroductionText8.Visibility = Visibility.Hidden;
+            IntroductionText9.Visibility = Visibility.Hidden;
+            NameBox.Visibility = Visibility.Hidden;
+            NameSubmitButton.Visibility = Visibility.Hidden;
+        }
+
+        private void ShowGameButtons()
+        {
+            RockButton.Visibility = Visibility.Visible;
+            PaperButton.Visibility = Visibility.Visible;
+            ScissorsButton.Visibility = Visibility.Visible;
         }
     }
 }
